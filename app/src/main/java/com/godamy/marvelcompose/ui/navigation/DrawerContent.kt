16a5a3f6aa1ba82e0798b1782ef031d91131d45e
Miguel Godoy <g.miguel27@gmail.com>
2022-06-27 23:05:10 +0200

    Composition Local Provider

--- app/src/main/java/com/godamy/marvelcompose/ui/navigation/DrawerContent.kt
+++ app/src/main/java/com/godamy/marvelcompose/ui/navigation/DrawerContent.kt
@@ -5,11 +5,9 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.ContentAlpha
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -52,27 +50,33 @@ fun DrawerContent(
         val selected = selectedIndex == index
         val colors = MaterialTheme.colors
 
-        Row(modifier = Modifier
-            .clickable { onOptionClick(navItem) }
-            .fillMaxWidth()
-            .padding(8.dp, 4.dp)
-            .clip(RoundedCornerShape(4.dp))
-            .background(
-                color = if (selected) colors.primary.copy(alpha = 0.12f) else colors.surface
-            )
-            .padding(12.dp)
+        val localContentColor = if (selected) colors.primary else colors.onBackground
+
+        CompositionLocalProvider(
+            LocalTextStyle provides MaterialTheme.typography.body1.copy(fontWeight = FontWeight.ExtraBold),
+            LocalContentColor provides localContentColor
         ) {
-            Icon(
-                imageVector = navItem.icon,
-                tint = if (selected) colors.primary else colors.onSurface.copy(alpha = ContentAlpha.medium),
-                contentDescription = navItem.name,
-            )
-            Spacer(modifier = Modifier.width(24.dp))
-            Text(
-                text = navItem.name,
-                color = if (selected) colors.primary else colors.onSurface,
-                style = MaterialTheme.typography.body1.copy(fontWeight = FontWeight.ExtraBold)
-            )
+            Row(modifier = Modifier
+                .clickable { onOptionClick(navItem) }
+                .fillMaxWidth()
+                .padding(8.dp, 4.dp)
+                .clip(RoundedCornerShape(4.dp))
+                .background(
+                    color = if (selected) LocalContentColor.current.copy(alpha = 0.12f) else colors.surface
+                )
+                .padding(12.dp)
+            ) {
+                CompositionLocalProvider(
+                    LocalContentAlpha provides ContentAlpha.medium
+                ) {
+                    Icon(
+                        imageVector = navItem.icon,
+                        contentDescription = navItem.name,
+                    )
+                }
+                Spacer(modifier = Modifier.width(24.dp))
+                Text(text = navItem.name)
+            }
         }
     }
 }
