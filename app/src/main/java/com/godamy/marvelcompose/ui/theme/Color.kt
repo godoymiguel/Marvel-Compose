bb4bf08a6bc34520f2a176218eafd6f0cbac7666
Miguel Godoy <g.miguel27@gmail.com>
2022-07-01 17:40:21 +0200

    Colors

--- app/src/main/java/com/godamy/marvelcompose/ui/theme/Color.kt
+++ app/src/main/java/com/godamy/marvelcompose/ui/theme/Color.kt
@@ -6,3 +6,8 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val Red300 = Color(0xFFE57373)
+val Red600 = Color(0xFFE53935)
+val Red900 = Color(0XFFB71C1C)
+val Blue800 = Color (0xFF1565C0)
