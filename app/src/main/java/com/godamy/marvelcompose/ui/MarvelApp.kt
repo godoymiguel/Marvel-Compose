8759d1a356436ef9c238426871ef5f307e0a399e
Miguel Godoy <g.miguel27@gmail.com>
2022-07-06 19:12:28 +0200

    Themes Module (#7)
    * Colors
    
    * System Colors with accompanist

--- app/src/main/java/com/godamy/marvelcompose/ui/MarvelApp.kt
+++ app/src/main/java/com/godamy/marvelcompose/ui/MarvelApp.kt
@@ -4,15 +4,22 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import com.godamy.marvelcompose.ui.navigation.AppBottomNavigation
 import com.godamy.marvelcompose.ui.navigation.DrawerContent
 import com.godamy.marvelcompose.ui.navigation.Navigation
+import com.godamy.marvelcompose.ui.screen.common.SetStatusBarColorEffect
 import com.godamy.marvelcompose.ui.screen.main.MainAppBar
 import com.godamy.marvelcompose.ui.screen.main.MarvelScreen
+import com.godamy.marvelcompose.ui.theme.Red900
 import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.systemuicontroller.SystemUiController
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 @ExperimentalFoundationApi
 @ExperimentalMaterialApi
@@ -48,5 +55,7 @@ fun MarvelApp() {
                 Navigation(marvelAppState.navController)
             }
         }
+
+        SetStatusBarColorEffect()
     }
 }
